52519ad61aeb618674bed231f4e9250836fc320d
Dmitry Legostaev <d.legostaev@a1qa.com>
2022-11-09 11:31:59 +0300

    Added EnumStepArgumentPlugin

---
+++ EnumStepArgumentConverter.SpecFlowPlugin/EnumStepArgumentPlugin.cs
@@ -0,0 +1,21 @@
+﻿using EnumStepArgumentConverter.SpecFlowPlugin;
+using TechTalk.SpecFlow.Bindings;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+// If you would like to use this plugin, please review it, as it have been copied from
+// https://github.com/clrudolphi/EnumDescriptionStepArgumentTransformer sample project made by Chris Rudolphi
+[assembly: RuntimePlugin(typeof(EnumStepArgumentPlugin))]
+
+namespace EnumStepArgumentConverter.SpecFlowPlugin;
+
+public class EnumStepArgumentPlugin : IRuntimePlugin
+{
+    public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+    {
+        runtimePluginEvents.CustomizeTestThreadDependencies += (sender, args) =>
+        {
+            args.ObjectContainer.RegisterTypeAs<CustomStepArgumentTypeConverter, IStepArgumentTypeConverter>();
+        };
+    }
+}
