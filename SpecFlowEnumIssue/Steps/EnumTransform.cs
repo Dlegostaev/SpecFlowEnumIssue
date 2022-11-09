c75c9875b697c1490851da2260b538d2d8dbb728
Dmitry Legostaev <d.legostaev@a1qa.com>
2022-11-09 11:32:53 +0300

    Deleted CustomEnumTransform and StepDefinition for it as they're no longer needed

--- SpecFlowEnumIssue/Steps/EnumTransform.cs
+++ SpecFlowEnumIssue/Steps/EnumTransform.cs
@@ -1,6 +1,4 @@
 ﻿using SpecFlowEnumIssue.Enum;
-using SpecFlowEnumIssue.Utils;
-using Xunit;
 
 namespace SpecFlowEnumIssue.Steps;
 
@@ -18,29 +16,6 @@ public sealed class EnumTransform
     public void WhenIChooseTestOptionsEnumOptionDefaultTransform(TestOptions option)
     {
         Console.WriteLine(option);  // No assert needed
-    }
-    
-    [When(@"I choose (.*) enum option - Custom Transform")]
-    public void WhenIChooseEnumOptionCustomTransform(string option)
-    {
-        TestOptions? optionEnumExpected = null;
-        switch (option)
-        {
-            case "Regular":
-                optionEnumExpected = TestOptions.Regular;
-                break;
-            case "Two Words":
-                optionEnumExpected = TestOptions.TwoWords;
-                break;
-            case "Special!Character":
-                optionEnumExpected = TestOptions.SpecialCharacter;
-                break;
-            case "Non Matching":
-                optionEnumExpected = TestOptions.NonSense;
-                break;
-        }
-        var optionEnumActual = option.AsEnum<TestOptions>();
-        Assert.Equal(optionEnumExpected, optionEnumActual);
     }
 
     [When(@"^I choose (.*) enum option - Default Transform Regex")]
